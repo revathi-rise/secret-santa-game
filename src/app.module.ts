6b8bf0a31f35596bde5944229de26d9694ce319d
Revathi S <revathys@technotackle.com>
2025-08-21 17:12:53 +0530

    removed upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SecretSantaModule } from './secret-santa/secret-santa.module';
-import { UploadModule } from './upload/upload.module';
-import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [EmployeesModule, UploadModule, SecretSantaModule],
+  imports: [SecretSantaModule],
   controllers: [AppController],
   providers: [AppService],
 })
